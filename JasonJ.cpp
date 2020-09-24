750442e0e213ca374b75854922741e40b707f4f9
JJellie1 <67670674+JJellie1@users.noreply.github.com>
2020-09-24 11:35:12 -0700

    Save functionality
    Code that has two functions. One takes a character object and a filename and saves the info stored in the object to the file. The other takes a filename and saves the data from the file into the object and returns the object.
    Used for saving game and loading a saved game.

---
+++ JasonJ.cpp
@@ -0,0 +1,42 @@
+#include "save.h"
+
+std::ostream & operator << (std::ostream &out, const character & in) {
+	out << in.name << "\n" << in.lvl << "\n" << in.maxHP << "\n" << in.HP << "\n";
+	for (int x = 0; x < 5; x++) {
+		out << in.stats[x] << " ";
+	}
+	return out;
+}
+
+std::istream & operator >> (std::istream & in, character & out) {
+	in >> out.name;
+	in >> out.lvl;
+	in >> out.maxHP;
+	in >> out.HP;
+	for (int x = 0; x < 5; x++) {
+		in >> out.stats[x];
+	}
+	return in;
+}
+
+void saveout(character & in, std::string txt) {
+	std::ofstream out;
+	out.open(txt);
+	if (out.is_open()) {
+		out << in;
+		out.close();
+	}
+	else std::cout << "Unable to open file";
+}
+
+character savein(std::string txt) {
+	std::ifstream in;
+	in.open(txt);
+	character temp = character();
+	if (in.is_open()) {
+		in >> temp;
+		in.close();
+	}
+	else std::cout << "Unable to open file";
+	return temp;
+}
