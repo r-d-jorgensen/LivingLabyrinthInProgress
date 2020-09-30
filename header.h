ce3ea4e3d865d8b708bf9c168a7b40f5647b95fd
JJellie1 <67670674+JJellie1@users.noreply.github.com>
2020-09-30 11:30:51 -0700

    Reuploaded header

---
+++ header.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+
+
+#include "char.h"
+#include "JasonJ.h"
+#include "ScottK.h"
+
+using namespace std;
