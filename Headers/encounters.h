89bc22a31a2846952a82180a58a7913ae2e10fd1
ScottKurtz <scottkurtz8373@gmail.com>
2020-10-24 13:30:22 -0700

    .h files added to Header folder

---
+++ Headers/encounters.h
@@ -0,0 +1,16 @@
+#pragma once
+#include<iostream>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+#include<fstream>
+#include "luckyEncounter.cpp"
+#include"gamblingEncounter.cpp"
+#include"trapEncounter.cpp"
+#include "majorEncounter.cpp"
+int riddle();
+int question();
+#include"strLower.cpp"
+#include"monsterEncounter.cpp"
+int encounterType(int gameLevel, int count);
+using namespace std;
