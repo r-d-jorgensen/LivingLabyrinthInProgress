539fc56562fca8cffc0390041b7153e1715b6a71
David <r.d.jorgensen@gmail.com>
2020-11-02 13:46:08 -0800

    menu system in place

--- FrameWork/textOutput.cpp
+++ FrameWork/textOutput.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 void dialogueLong(string str, string startStr, int paddingLength, string padding);
 void dialogue(string str, int msgType, string speaker);
+void Menu(string menuName, string optionsStr[][2], int optionsNum, int menuType);
 
 void dialogueLong(string str, string startStr = "", int paddingLength = 0, string padding = "")
 {
@@ -96,7 +97,6 @@ void dialogue(string str, int msgType = 0, string speaker = "Self")
         }
         cout << "\t" + edge << endl
              << edge + internalSpace + internalSpace + internalSpace + edge;
-        break;
     }
     default:
         cout << "Something is wrong with the save file textType.\n";
@@ -104,13 +104,63 @@ void dialogue(string str, int msgType = 0, string speaker = "Self")
     cout << endl;
     return;
 }
-/*
+
+void Menu(string menuName, string optionsStr[][2], int optionsNum, int menuType = 0)
+{
+    int textType = 1; //import from save file
+    switch (textType)
+    {
+    //bare format
+    case 0:
+        cout << menuName << endl;
+        for (int i = 0; i < optionsNum; i++)
+        {
+            if (optionsStr[i][1].length() > 78)
+            {
+            }
+            cout << optionsStr[i][0] << ": " + optionsStr[i][1] << endl;
+        }
+        break;
+    //simple format
+    case 1:
+        cout << "\t" << menuName << endl;
+        cout << "================================================" << endl;
+        for (int i = 0; i < optionsNum; i++)
+        {
+            if (optionsStr[i][1].length() > 78)
+            {
+            }
+            cout << "\t" << optionsStr[i][0] << ": " + optionsStr[i][1] << endl;
+        }
+        cout << "================================================" << endl;
+        break;
+    //stylized format
+    case 2:
+        //not implemented yet
+        break;
+    }
+    cout << endl;
+    return;
+}
+/* testingSystems
 int main()
 {
     dialogue("Hello world");
     dialogue("Rumble" , 1, "Enviroment");
     dialogue("It cannot be seen, cannot be felt, cannot be heard, and cannot be smelt. It lies behind stars and under hills, And empty holes it fills. It comes first and follows after, Ends life, and kills laughter. What is it?", 2, "NPC");
     
+    int menuOptions = 5;
+    string menu[menuOptions][2] =
+    {
+        {"1", "Talk to book NPC"},
+        {"2", "Go to perk store"},
+        {"3", "Open char sheet"},
+        {"4", "Settings Menu"},
+        {"5", "Exit the game"}
+    };
+    Menu("Living Libary Menu", menu, menuOptions);
+
+    //size checker
     for (int i = 0; i < 8; i++)
         for (int j = 0; j < 10; j++)
             cout << j;
