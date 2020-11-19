bfad7f70e8882de1e31ef5f877b2ea08b7cbad22
ScottKurtz <scottkurtz8373@gmail.com>
2020-11-18 20:21:20 -0800

    added program.cpp and camp.cpp to ScottK in OfficialVersion

--- OfficialVersion/ScottK.cpp
+++ OfficialVersion/ScottK.cpp
@@ -3,6 +3,8 @@
 #include<string>
 #include<fstream>
 using namespace std;
+int program();
+void camp();
 string strLower(string n);
 int riddle();
 int question();
@@ -14,6 +16,140 @@ void printGreeting();
 int doorGame();
 void majorEncounter();
 #include"DavidJ.cpp"
+
+/* describesLiving Labyrinth, gives instructions if requested by player */
+int program() 
+{
+	dialogue("Welcome to the Living Labyrinth");
+	dialogue("You have been selected because of your superior knowledge and ");
+	dialogue("intellect in the  computer software field to take part in the");
+	dialogue("alpha testing of the Living Labyrinth. Thank you for your");
+	dialogue("willingness to participate in this endeavor.");
+	dialogue("if you would like more background or instructions, please press '1'");
+	dialogue("Otherwise,press '2' to start playing ");
+	int choice = 0;
+	cin >> choice;
+	while ((cin.fail()) || !(choice >= 1 && choice <= 2)) {
+		cin.clear();
+		cin.ignore();
+		dialogue("Please choose either 1 or 2");
+		cin >> choice;
+	}
+	if (choice == 1) {
+		dialogue("The Living Labyrinth is primarily a menu driven,");
+		dialogue("single player, fantasy RPG. One exciting innovation");
+		dialogue("we have implemented is the option for users to choose a game that"); 
+		dialogue("includes a lot of graphics that are visually appealing to most");
+		dialogue("players but there is an additional option that is text based,");
+		dialogue("which is more user friendly for the blind and visually impaired. ");
+		dialogue("This iteration will only have bare, which is text only, and sparse,");
+		dialogue("which has a few visuals and places text in various spots on screen");  
+		dialogue("You will be asked your preference after this explanation");
+		dialogue("Another exciting aspect of the game is that you do not have to");
+		dialogue("finish it in one sitting. You can stop the game and your character");
+		dialogue("will be saved.");
+		dialogue("When you return to your game you will start at the last  check");
+		dialogue("point you passed. You will find yourself  in the library after");
+		dialogue("the last letter clue that you procured. You will keep your items,");
+		dialogue("gold, and experience.");
+		dialogue("At the end of reading this explanation, you will be asked to enter");
+		dialogue("the level you would like to play. This level represents the");
+		dialogue("length of the anagram that you will have to solve to finish");
+		dialogue("the quest. Level 1 is a 3 letter anagram while level 7 is a 9 ");
+		dialogue("letter anagram. As you travel throughout the land you will run ");
+		dialogue("into traps, monsters, lucky finds and opportunities to gamble your");
+		dialogue("money away. the main goal is to find the 'Riddle Masters' that are");
+		dialogue("gatekeepers of the rings with monograms on them. You will be given");
+		dialogue("a riddle, a question, and choice of 3 dorrs to go through. How ");
+		dialogue("well you do on these 3 tasks will determine the difficulty level ");
+		dialogue("of the monster you will have to kill in order to find a ring ");
+		dialogue("with a letter engraved in it. If you answer both questions");
+		dialogue("correctly and choose the right door, then you will find the ring ");
+		dialogue("without having to fight anything. Once you find a ring, you will go");
+		dialogue("back to the library and continue on your journey to find the next ");
+		dialogue("ring. When you find all of the rings with letters engraved in them");
+		dialogue("then you will have to fight a Boss and solve the anagram to win");
+		dialogue("that round. ");
+		dialogue("This is the first iteration of our, 'Living Labyrinth,' game. You");
+		dialogue(" will notice that we only have one Book Realm at this time and that");
+		dialogue("some of the menu choices currently lead to dead ends. As the");
+		dialogue("alpha testers of our program, we want you to concentrate on the ");
+		dialogue("character saving ability of the program, the bare");
+		dialogue("vs. 				sparse visual options, and the ");
+		dialogue("various encounters");
+		dialogue("Thank you in advance for helping us find any bugs that we may have");
+		dialogue("overlooked, so we can fix them while implementing additional");
+		dialogue("functionality in our next iteration. ");
+		dialogue("After choosing your anagram level, you will be in the library.");
+		dialogue("In the library, you can choose among:");
+		dialogue("Talk to NPC, Perk store, Character sheet, and Settings"); 
+		dialogue("If you want to change from graphical to text-based, go to settings");
+		dialogue("and make the change. After that, you may want to look at your ");
+		dialogue("character sheet or check out the perk store.");
+		dialogue("In order to start your adventure, select 'Talk to NPC' ");
+		dialogue("then select 'go to book realm'");
+		dialogue("In the book realm you can go to the armory or the general store.");
+		dialogue("In order to start the encounters, choose 'Explore and then choose");
+		dialogue("to move in any direction and the encounters will begin. You can");
+		dialogue("also choose to camp to restore some health. Of course,");
+		dialogue("you could get ambushed!");
+		dialogue("Good Luck!");
+	}
+	string charName;
+	dialogue("Okay, please enter the name of your character.");
+	cin >> charName;
+	dialogue("Your character's name is " + charName); // player.name = charName
+	int graphics;
+	dialogue("Choose your graphics level. '0' for bare, '1' for sparse, full N/A");
+	cin >> graphics;
+	while ((cin.fail()) || !(graphics >= 0 && graphics <= 1)) {
+		cin.clear();
+		cin.ignore();
+		dialogue("Please choose 0 or 1");
+		cin >> graphics;
+	}
+	dialogue("graphics level  is " + to_string(graphics)); // player.textTYpe = graphics;
+	dialogue("Now, please enter the level of ");
+	dialogue("difficulty you want to play (1 to 7'?");
+	int level;
+	cin >> level;
+	while ((cin.fail()) || !(level >= 1 && level <= 7)) {
+		cin.clear();
+		cin.ignore();
+		dialogue("Please choose 1 to 7");
+		cin >> level;
+	}
+	return level;
+}
+
+/* camp function is called when player wants to rest. there is a 10% chance 
+ * that a monster will ambush the player, so no rest is attained. Otherwise, if
+ *  allowed to rest, then player increases hit points by a constant factor 
+ *  based on constitution and a random number*/ 
+void camp()
+{
+	int randEnc = ((rand() + time(0)) % 10);
+	if (randEnc == 1){
+		dialogue("You hear a noise that wakes you from your slumber");
+		dialogue("You prepare for some kind of attack.");
+		monsterEncounter(100); //call monsterEncounter function
+	} else {
+		int maxHP = 40; // call player.maxHp;
+		int currentHP = 30; //call player.hp;
+		int diff = maxHP - currentHP;
+		int constitution = 7; // call player.con
+		int randNum = ((rand() + time(0)) % (maxHP/2));
+		int conAdjustment = constitution * (maxHP/20); // incorporates player con to heal 
+		int hpFactor = conAdjustment + randNum;
+		currentHP = currentHP + ((diff * hpFactor) / maxHP);
+
+		dialogue("You feel rested. You gained ");
+		dialogue(to_string((diff * hpFactor) / maxHP) + "HP");
+		dialogue("You have " + to_string(currentHP) + "HP");
+		// player.setHP(currentHP);
+	}
+	return;
+}
 
 /* function takes in a string and converts all letters to lower case so
  * when comparing player's guess to the answer, the case doesn't matter */ 
@@ -29,12 +165,12 @@ void printgreeting()
 {
 	string greeting[5] = {
 		"Hello, here is a riddle for you.",
-			"It is good to see you. I have a riddle for you.",
-			"I have been waiting for you. Here is a mind bender for you.",
-			"If you answer this question correctly, I will help you pick the best door.",
-			"There you are! Answer this."};
-			int randNum = (rand() + time(0)) % 5;
-			dialogue(greeting[randNum], 2, "Greeter");
+		"It is good to see you. I have a riddle for you.",
+		"I have been waiting for you. Here is a mind bender for you.",
+		"If you answer this question correctly, I will help you pick the best door.",
+		"There you are! Answer this."};
+	int randNum = (rand() + time(0)) % 5;
+	dialogue(greeting[randNum], 2, "Greeter");
 }
 /* random chance to help determine the level of the monster to battle 
  * a wrong selection increases the difficulty by 2 levels  */
@@ -46,7 +182,7 @@ int doorgame()
 	int randNum = (rand() + time(0)) % 3;
 	dialogue("Choose a door: Left, Middle, or Right", 2, "Door Master");
 	cin >> guess;
-/* call to convert to lower case for comparison */
+	/* call to convert to lower case for comparison */
 	guess = strLower(guess);
 	if (guess == door[randNum]) {
 		monsterlevel = 0;
@@ -61,76 +197,99 @@ int doorgame()
  * a 1 increasing the monster difficulty by 1 at end of major encounter */ 
 int riddle()
 {
-    ifstream riddleFile;
-    riddleFile.open("riddles.txt");
-    string randomRiddle;
-    string guess;
-    string answer;
-    int monsterlevel; //int returned by function to determine monster difficulty
-    int num = ((rand() + time(0)) % 63);
-    num = 2*num;
-    for (int i=0;i<num+1;i++) 
-        getline(riddleFile, randomRiddle);
-    dialogue((randomRiddle) + " ",2 , "Riddle Master");
-    getline(riddleFile, answer);
-    answer = strLower(answer);
-    answer.erase(answer.find_last_not_of("\n\r") + 1);
-    getline(cin, guess);
-    guess.erase(guess.find_last_not_of("\n\r") + 1);
-    /* function call to convert all letters to lowercase, so simple capitalization doesn't cause a wrong answer */
-    guess = strLower(guess);
-    /* if wrong then get more difficult monster otherwise no monster or easier monster */
-    if (guess.compare(answer) == 0) {
-        dialogue("Good job. Go through that door andd meet the Question Master", 2, "Riddle Master");
-        monsterlevel = 0;
-    }	else	{
-        dialogue("Sorry, that is incorrect but go ahead through that door");
-       dialogue("Talk to the Question Master.", 2, "Riddle Master");
-        monsterlevel = 1;
-    }
-    riddleFile.close();
-    return monsterlevel;
+	ifstream riddleFile;
+		riddleFile.open("riddles.txt");
+		string randomRiddle;
+		string guess;
+		string answer;
+		int monsterlevel; //int returned by function to determine monster difficulty
+	int num = ((rand() + time(0)) % 63);
+		num = 2*num;
+		for (int i=0;i<num+1;i++) 
+			getline(riddleFile, randomRiddle);
+				dialogue((randomRiddle) + " ",2 , "Riddle Master");
+				getline(riddleFile, answer);
+				answer = strLower(answer);
+				answer.erase(answer.find_last_not_of("\n\r") + 1);
+				getline(cin, guess);
+				guess.erase(guess.find_last_not_of("\n\r") + 1);
+				/* function call to convert all letters to lowercase, so simple capitalization doesn't cause a wrong answer */
+				guess = strLower(guess);
+				/* if wrong then get more difficult monster otherwise no monster or easier monster */
+				if (guess.compare(answer) == 0) {
+					dialogue("Good job. Go through that door andd meet the Question Master", 2, "Riddle Master");
+						monsterlevel = 0;
+				}	else	{
+					dialogue("Sorry, that is incorrect but go ahead through that door");
+						dialogue("Talk to the Question Master.", 2, "Riddle Master");
+						monsterlevel = 1;
+				}
+	riddleFile.close();
+		return monsterlevel;
 } // end riddle function
 int question()
 {
 	ifstream questionFile;
-	questionFile.open("questions.txt");
-	string randomQuestion;
-	string guess;
-	string answer;
-	int monsterlevel; //int returned by function to determine monster difficulty
-	int randNum = ((rand() + time(0)) % 5);
-	randNum = 2*randNum;
-	for (int i=0;i<randNum+1;i++) 
-		getline(questionFile, randomQuestion);
-	dialogue((randomQuestion) + " ",2, "Question Master");
-	getline(questionFile, answer);
-	answer = strLower(answer);
-	answer.erase(answer.find_last_not_of("\n\r") + 1);
-	getline(cin, guess);
-	guess.erase(guess.find_last_not_of("\n\r") + 1);
-	/* function call to convert all letters to lowercase, so simple capitalization doesn't cause a wrong answer */
-	guess = strLower(guess);
-	/* if wrong then get more difficult monster otherwise no monster or easier monster */
-	if (guess.compare(answer) == 0) {
-		dialogue("Good job. Go through that door andd meet the Door Master", 2, "Question Master");
-		monsterlevel = 0;
-	} else {
-		dialogue("Sorry, that is incorrect.");
-				dialogue("Go through that door and talk to the Door Master", 2,"Question Master");
-				monsterlevel = 1;
+		questionFile.open("questions.txt");
+		string randomQuestion;
+		string guess;
+		string answer;
+		int monsterlevel; //int returned by function to determine monster difficulty
+	int randNum = ((rand() + time(0)) % 47);
+		randNum = 2*randNum;
+		for (int i=0;i<randNum+1;i++) 
+			getline(questionFile, randomQuestion);
+				dialogue((randomQuestion) + " ",2, "Question Master");
+				getline(questionFile, answer);
+				answer = strLower(answer);
+				answer.erase(answer.find_last_not_of("\n\r") + 1);
+				getline(cin, guess);
+				guess.erase(guess.find_last_not_of("\n\r") + 1);
+				/* function call to convert all letters to lowercase, so simple capitalization doesn't cause a wrong answer */
+				guess = strLower(guess);
+				/* if wrong then get more difficult monster otherwise no monster or easier monster */
+				if (guess.compare(answer) == 0) {
+					dialogue("Good job. Go through that door andd meet the Door Master", 2, "Question Master");
+						monsterlevel = 0;
+				} else {
+					dialogue("Sorry, that is incorrect.");
+						dialogue("Go through that door and talk to the Door Master", 2,"Question Master");
+						monsterlevel = 1;
 				}
-				questionFile.close();
-				return monsterlevel;
-				} // end question function
-/* monster function selects monster */
+	questionFile.close();
+		return monsterlevel;
+} // end question function
+
+/* function receives an int which indicates level of the monster if received 
+ * from majorEncounter. A 100  indicates that monsterEncounter was called 
+ * from Explore. function adjusts monster using randomization and chaar's lvl */ 
 void monsterEncounter(int level)
-{ cout << "there is a monster at level " << level << endl;
-	/* I think that maybe we use this function to find a monster from the  monster.txt file,
-	 * create the armor class, hp and everything and then pass those attributes on  to
-	 * the monstercombat function with all of the parameters passes to it. not sure */
-	// call to monsterCombat function?
+{ 
+	int monsterLevel = 0;
+	if (level == 100) {
+		int randNum = ((rand() + time(0)) % 100);
+		if (randNum < 100)
+			monsterLevel = 1;
+		if (randNum <94)
+			monsterLevel = 0;
+		if (randNum < 87)
+			monsterLevel = -1;
+		if (randNum < 75)
+			monsterLevel = -2;
+		if (randNum < 50)
+			monsterLevel = -3;
+	} else {
+		int randNum = ((rand() + time(0)) % 3);
+		monsterLevel = level + randNum -1;
+	}
+	int charLevel = 1; // call char.lvl();
+	monsterLevel += charLevel;
+	//	 monster = new monster(monsterLevel);
+	//monsterCombat();
+	cout << "Monster sent to combat is level " << monsterLevel << endl;
 }
+
+
 /* lucky encounter  function selects a random lucky find
  * gold, weapon, armor, or artifact */
 void luckyEncounter()
@@ -349,13 +508,13 @@ void majorEncounter()
 		dialogue("In the back of the room you see a golden letter 'S'");
 }
 /*
-int main()
-{
-luckyEncounter();
-gamblingEncounter();
-trapEncounter();
-monsterEncounter(3);
-majorEncounter();
-return 0;
-}
-*/
+   int main()
+   {
+   luckyEncounter();
+   gamblingEncounter();
+   trapEncounter();
+   monsterEncounter(3);
+   majorEncounter();
+   return 0;
+   }
+ */
