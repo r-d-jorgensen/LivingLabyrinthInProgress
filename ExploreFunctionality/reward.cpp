ba732b3b5f2aefee80c011dc1417d320fb1c28d1
ScottKurtz <scottkurtz8373@gmail.com>
2020-11-24 23:11:41 -0800

    reward send independent

--- ExploreFunctionality/reward.cpp
+++ ExploreFunctionality/reward.cpp
@@ -1,8 +1,26 @@
-#pragma once
-#include <iostream>
-using namespace std;
-
-void reward()
+/* reward function generates the reward depending on the type of monster
+ * killed and where the monstr was met i.e. in wild or in major encounter */
+void reward(string type, int amount)
 {
-    return;
+	int gold = 0;
+	if (type == "wild") {
+		for (int i = 0;i<amount;i++) {
+			int randNum = ((rand() + time(0)) % 5);
+			gold += randNum;
+		}
+	}
+	else if (type == "clue") {
+		gold = amount * 100;
+		for (int i = 0;i < amount;i++) {
+			int randNum = ((rand() + time(0)) % 50);
+			gold += randNum;
+		}
+	}
+	else {
+		gold =2;
+	}	
+	dialogue("You found " + to_string(gold) + "gold pieces after killing the enemy");
+	// ### player.gold = gold;
+	return;
 }
+
